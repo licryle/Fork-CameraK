d39f79136083c2a1054dde8834c30c8567d532fa
Licryle <licryle@users.noreply.github.com>
2025-10-13 01:09:34 +0800

    Fixing macOS gradle issue.

--- cameraK/build.gradle.kts
+++ cameraK/build.gradle.kts
@@ -39,12 +39,6 @@ kotlin {
         }
     }
 
-    targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
-        binaries.framework {
-            baseName = "cameraK"
-        }
-    }
-
     sourceSets {
         val desktopMain by getting {
             dependencies {
